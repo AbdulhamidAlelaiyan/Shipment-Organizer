218c30054c6ca163ab2777e12772ddabb965fa83
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-02-23 12:57:50 +0300

    Created the Shipments Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import Shipments from "./components/Shipments";
 
 class App extends Component {
     render() {
         return (
             <>
-                <h1>React</h1>
+                <h1>Shipment Organizer</h1>
+                <Shipments/>
             </>
         );
     }
---
+++ src/components/Shipments.js
@@ -0,0 +1,11 @@
+import React, {useLayoutEffect} from 'react';
+
+export default class Shipments extends React.Component {
+    render() {
+        return (
+          <>
+              ShipmentS Component
+          </>
+        );
+    }
+}
