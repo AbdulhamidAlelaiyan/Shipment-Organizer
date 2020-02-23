1a53a3870dc8fede24812ce5a02fe5a750a32265
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-02-23 13:17:26 +0300

    Initial bootstrap styling added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import Shipments from "./components/Shipments";
+import ShipmentTracker from "./components/ShipmentTracker";
+import { Container, Col, Row } from 'reactstrap';
 import axios from 'axios';
 
 class App extends Component {
     render() {
         return (
-            <>
-                <h1>Shipment Organizer</h1>
-                <Shipments/>
-            </>
+            <Container>
+                <h1 className='text-center'>📦Shipments Organizer📦</h1>
+                <Row>
+                    <Col><Shipments title='shipments on the way'/></Col>
+                    <Col><Shipments title='shipments delivered'/></Col>
+                </Row>
+                <hr/>
+                <ShipmentTracker title='shipment checkpoint' shipmentId={null}/>
+            </Container>
         );
     }
 }
--- src/components/ShipmentTracker.js
+++ src/components/ShipmentTracker.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Badge, ListGroup, ListGroupItem} from "reactstrap";
 
 export default class ShipmentTracker extends React.Component {
     render() {
         return (
           <>
-              ShipmentTracker
+              <ListGroup>
+                  <ListGroupItem className="justify-content-between">Cras justo odio <Badge pill>14</Badge></ListGroupItem>
+                  <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <Badge pill>2</Badge></ListGroupItem>
+                  <ListGroupItem className="justify-content-between">Morbi leo risus <Badge pill>1</Badge></ListGroupItem>
+              </ListGroup>
           </>
         );
     }
